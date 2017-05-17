e3287140034f0348676c14230f143a668089011c
Ben Laurin <benlaurin@Ben-Laurin.local>
2017-05-17 10:41:48 -0400

    finito gang gang gang

--- ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise 2.xcplaygroundpage/Contents.swift
+++ ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise 2.xcplaygroundpage/Contents.swift
@@ -23,22 +23,29 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
 
+canvas.fillColor = Color(hue: 83, saturation: 8, brightness: 85, alpha: 100)
+canvas.drawRectangle(centreX: 200, centreY: 300, width: 400, height: 600)
 //make all the text at the top
 
 //variable so that if the textsize needs to be changed i dont have to replace every single one
 var textsize = 10
 var yvalupper = 550
 //upper text
-canvas.textColor = Color.black
+canvas.textColor = Color.red
 canvas.drawText(message: "straight music presents", size: textsize, x: 20, y: yvalupper)
 canvas.drawText(message: "victoria hall hanley", size: textsize, x: 150, y: yvalupper)
 canvas.drawText(message: "advance tickets: $2.00", size: textsize, x: 280, y: yvalupper)
-////lower text
-//canvas.drawText(message: "october 16 1993 / 8 pm", size: textsize, x: 20, y: 560)
-//canvas.drawText(message: "at university of sussex", size: textsize, x: 150, y: 560)
-//canvas.drawText(message: "salad, plus my life story", size: textsize, x: 280, y: 560)
-////price text
-//canvas.drawText(message: "$6.50 / &5.50 advance", size: textsize, x: 280, y: 550)
+//lower text
+canvas.drawText(message: "the undertones", size: textsize, x: 20, y: 540)
+canvas.drawText(message: "stroke-on-trent, england", size: textsize, x: 150, y: 540)
+canvas.drawText(message: "at the door: $2.50", size: textsize, x: 280, y: 540)
+
+//second layer
+canvas.drawText(message: "with special guests", size: textsize, x: 20, y: 520)
+canvas.drawText(message: "tuesday", size: textsize, x: 150, y: 520)
+//lower text
+canvas.drawText(message: "the chords", size: textsize, x: 20, y: 510)
+canvas.drawText(message: "june 5 1979 / 7:30 pm", size: textsize, x: 150, y: 510)
 
 
 
@@ -46,10 +53,11 @@ canvas.translate(byX: 10, byY: 400)
 
 
 for y in stride(from: 95, to: 0, by: -5){
+      canvas.textColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: y)
     canvas.drawText(message: "undertones", size: 70, x: 0, y: 0)
     canvas.translate(byX: -2, byY: 0)
     canvas.rotate(by: -5)
-    canvas.textColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: y)
+  
 }
 
 
